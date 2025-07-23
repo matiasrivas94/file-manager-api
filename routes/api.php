82b3c49f4e68fef9b7469fa570663c7c631ad8c2
Matias Rivas <matiasrivas202@gmail.com>
2025-07-22 21:33:35 -0300

    Rutas Api creadas

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\FolderController;
+use App\Http\Controllers\API\FileController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// RUTA TEST
+Route::get('/', fn() => response()->json(['message' => 'API funcionando correctamente!']));
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// CARPETAS
+Route::apiResource('folders', FolderController::class);
+
+// ARCHIVOS
+Route::apiResource('files', FileController::class);
+
+// RESTAURAR ARCHIVO ELIMINADO
+Route::post('files/{id}/restore', [FileController::class, 'restore']);
+
+// ELIMINAR DEFINITIVAMENTE UN ARCHUVO
+Route::delete('files/{id}/force', [FileController::class, 'forceDelete']);
